1ab70c894f49c3ade721a5f47e4a0308b06e341f
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2019-05-18 19:05:30 +0300

    'Solution3'

--- src/js/main.js
+++ src/js/main.js
@@ -5,24 +5,19 @@ let counter =0;
 counter += 260;
 
 function arrowRight() {
-    console.log(arrowRight);
     counter += 260;
     if (counter <= 1040) {
-        element.style.right = counter + 'px';
-    } else if (element.contains.style === undefined){
-        return;
-    }
-}
-
-
-function arrowLeft() {
-    counter -= 260;
-    element.style.right = counter + 'px';
-
-    if (element.contains.style.right > 0 ) {
         element.style.right = counter + 'px';
     } else {
         return;
     }
-  
+}
+
+function arrowLeft() {
+    counter -= 260;
+    if (element.style.right > 0) {
+        element.style.right = counter + 'px';
+    } else {
+        return;
+    }
 }
