13497d9b30fbcee6a3fa4331df86e638759658e0
StasGostev <spaceomen@gmail.com>
2019-05-18 16:52:49 +0300

    'fixed_bug_with_double_clicking_on_animation'

--- src/main.js
+++ src/main.js
@@ -1,18 +1,23 @@
 const buttons = Array.from(document.querySelectorAll(".arrow"));
 const imgContainer = document.querySelector(".img_container");
+const width = imgContainer.querySelector("img").width;
+const imgLength = imgContainer.querySelectorAll("img").length;
+const quantityPictures = document.querySelector('.img_wrapper').offsetWidth / width;
+const bezier = "cubic-bezier(.56,-1.99,.64,1.99)";
+
 
 buttons.forEach(elem => elem.addEventListener("click", slide, false));
 
 function slide(event) {
-  const direction = getAttribute(event.target, "direction");
+  
+  event.target.removeEventListener('click', slide);
+  
   const position = getOffsetX(imgContainer);
-  const width = imgContainer.querySelector("img").width;
-  const imgLength = imgContainer.querySelectorAll("img").length;
-  const quantityPictures = document.querySelector('.img_wrapper').offsetWidth / width;
-  const bezier = "cubic-bezier(.56,-1.99,.64,1.99)";
+  const direction = getAttribute(event.target, "direction");
   const step = width * quantityPictures;
   const stepRight = Math.max(position - step, -width * (imgLength - quantityPictures));
   const stepLeft = Math.min(position + step, 0);
+
 
   switch (direction) {
     case "left":
@@ -28,7 +33,9 @@ function slide(event) {
     default:
       break;
   }
-  console.log(document.querySelector('.img_wrapper').scrollWidth + ' after');
+  imgContainer.addEventListener('transitionend', () => {
+    buttons.forEach(elem => elem.addEventListener("click", slide, false));
+  })
 }
 
 function getAttribute(elem, attribute) {
