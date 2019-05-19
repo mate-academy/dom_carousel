90375b2380b7af6af8dd90f17a592b723c4c4315
Andrii <gaarasandy@gmail.com>
2019-05-19 22:14:14 +0300

    class add disabled

--- js/main.js
+++ js/main.js
@@ -2,9 +2,19 @@ document.addEventListener('DOMContentLoaded', function(){
   let next = document.querySelector('.next');
   next.onclick = function() {
     document.querySelector(".container").scrollLeft += 390;
+    if (document.querySelector(".container").scrollLeft >= 900) {
+      next.classList.add('disabled');
+    }else if (document.querySelector(".container").scrollLeft >= 390) { 
+      prev.classList.remove('disabled');
+    }
   };
   let prev = document.querySelector('.prev');
   prev.onclick = function() {
     document.querySelector(".container").scrollLeft -= 390;
+    if (document.querySelector(".container").scrollLeft < 900 && document.querySelector(".container").scrollLeft > 391) {
+      next.classList.remove('disabled');
+    }else if (document.querySelector(".container").scrollLeft === 0) {
+      prev.classList.add('disabled');
+    }
   };
 }, false );
