a333807f9cbf87f66492f7268b7ff78d90c4b545
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-05-19 20:10:03 +0300

    solution

---
+++ script.js
@@ -0,0 +1,43 @@
+const items = document.querySelector('.items');
+const carousel = document.querySelector('.carousel');
+const buttons = document.querySelector('.buttons');
+const btnNext = document.querySelector('.next');
+const btnPrev = document.querySelector('.prev');
+
+checkScroll();
+
+buttons.onclick = function (e) {
+    const target = e.target;
+    console.log(carousel.scrollLeft);
+    if(target.nodeName === 'BUTTON') {
+        scroll(items, target.dataset.action);
+    }
+};
+
+function scroll(elem, action) {
+    if(action === 'next') {
+        carousel.scrollLeft += 390;
+    }
+    if (action === 'prev') {
+        carousel.scrollLeft -= 390;
+    }
+    checkScroll();
+}
+
+
+function checkScroll() {
+    if (carousel.scrollLeft === 0) {
+        btnPrev.disabled = true;
+        btnPrev.style.opacity = "0.5";
+    } else {
+        btnPrev.disabled = false;
+        btnPrev.style.opacity = "1";
+    }
+    if (carousel.scrollLeft > 909.5) {
+        btnNext.disabled = true;
+        btnNext.style.opacity = "0.5";
+    } else {
+        btnNext.disabled = false;
+        btnNext.style.opacity = "1";
+    }
+}
