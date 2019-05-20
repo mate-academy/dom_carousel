857742599617a849990a7e837377f6d8da54207f
annakondr <tyukondra@gmail.com>
2019-05-20 20:12:52 +0300

    fixed_disabled

--- main.js
+++ main.js
@@ -3,22 +3,27 @@
 let leftArrow = document.querySelector('.previous');
 let rightArrow = document.querySelector('.next');
 let count = 0;
-
-leftArrow.addEventListener('click', function(){
-    document.querySelector('.galery').scrollLeft -= 420;
-    count -= 1;
-});
-
-rightArrow.addEventListener('click', function(){
+leftArrow.classList.add('disabled')
+rightArrow.addEventListener('click', function(event){
+    count += 420;
     document.querySelector('.galery').scrollLeft += 420;
-    count += 1;
-
+    if (count >= 420) {
+        leftArrow.classList.remove('disabled');
+    }
+    if (count >= 1260) {
+        count = 1260;
+        rightArrow.classList.add('disabled');
+    }
 });
 
-if (count <= 0) {
-    leftArrow.classList.add('disabled');
-    rightArrow.classList.remove('disabled')
-} else if (count > 1) {
-    rightArrow.classList.add('disabled');
-    leftArrow.classList.remove('disabled')
-}
+leftArrow.addEventListener('click', function(event) {
+    count -= 420;
+    document.querySelector('.galery').scrollLeft -= 420;
+    if (count <= 0) {
+        count = 0;
+        leftArrow.classList.add('disabled');
+    }
+    if (count <= 840) {
+        rightArrow.classList.remove('disabled');
+    }
+});
