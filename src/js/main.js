bbd899399cde4e4e2650b8a57ba8efafe16a8428
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2019-05-23 14:53:26 +0300

    'Sol'

--- src/js/main.js
+++ src/js/main.js
@@ -2,23 +2,23 @@ document.getElementById('previous').onclick = arrowLeft;
 document.getElementById('next').onclick = arrowRight;
 let element = document.getElementsByClassName('carusel_slides')[0];
 let counter =0;
-counter += 260;
 
 function arrowRight() {
-    counter += 260;
-    if (counter <= 1040) {
-        element.style.right = counter + 'px';
-    } else {
-        return;
-    }
+  counter += 260;
+  if(counter > 1040) counter = 1040;
+  if (counter <= 1040) {
+     element.style.right = counter + 'px';
+  } 
+  return;
 }
 
 function arrowLeft() {
-    let styleRight = element.style.right
-    counter -= 260;
-    if ( parseInt(styleRight.replace(/\D+/g,"")) > 0) {
-        element.style.right = counter + 'px';
-    } else {
-        return;
-    }
+  let styleRight = element.style.right;
+  counter -= 260;
+  if(counter < 0) counter = 0;
+  if ( parseInt(styleRight.replace(/\D+/g,"")) > 0) {
+      element.style.right = counter + 'px';
+  } 
+  return;
 }
+
