8f82a7cfbbc55583481b2add1dadb83a81a8d31e
Antonenko Bohdan <bohdan1997@gmail.com>
2019-05-28 17:12:11 +0300

    'changes'

--- 2.js
+++ 2.js
@@ -2,18 +2,17 @@ const left = document.querySelector('.left_button');
 const right = document.querySelector('.right_button');
 const list = document.querySelector('.imgList');
 
-left.addEventListener('click', function(e){
-    right.disabled = false;
-    if (list.scrollLeft === 910) {
-        left.disabled = true;
-    }
-    list.scrollLeft += 390;
-});
-
-right.addEventListener('click', function(e){
+right.addEventListener('click', function(){
     left.disabled = false;
-    if (right.scrollLeft === 0) {
+    list.scrollLeft += 390;
+    if (list.scrollLeft === 910) {
         right.disabled = true;
+    }
+});
+left.addEventListener('click', function(){
+    right.disabled = false;
+    if (list.scrollLeft === 0) {
+        left.disabled = true;
     }
     list.scrollLeft -= 390;
 });
